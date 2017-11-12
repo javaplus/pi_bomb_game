81316ff783b9bfab99978b0adeb2c85358df9473
javaplus <btarlton@gmail.com>
2017-11-12 21:19:39 +0000

    almost done... sends say command

--- bomb_controller.py
+++ bomb_controller.py
@@ -5,10 +5,16 @@ import button_blink_thread
 import setbomb
 from subprocess import Popen
 import subprocess
+import os
+import signal
+import time
+import urllib2
+import json
 
 running_thread = {}
 timer_process_id = None
 defuse_button = None
+time_delay = 1 
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -26,24 +32,50 @@ def on_message(client, userdata, msg):
     print(msg.topic+" Message:Payload="+str(msg.payload))
     global running_thread
     global defuse_button
+    global time_delay
     if msg.payload == "master_switch_ON": 
     	blink()
     if msg.payload == "master_switch_OFF":
         for key in running_thread:
        	    running_thread[key].stop()	
-    if msg.payload.startswith("button"):
+    if msg.payload.startswith("button") and time_delay < time.time():
 	print("Button press event detected!")
 	global timer_process_id
 
 	if defuse_button is None:
-		timer_process_id = Popen(["sudo", "python","/home/pi/workspace/pi_timer_python/mqtt_server.py"]).pid 
+		timer_process_id = Popen(["sudo", "python","/home/pi/workspace/pi_timer_python/mqtt_server.py"],preexec_fn=os.setsid).pid 
 		print("timer process=" + str(timer_process_id))
 		setbomb.submitTime()	  
 		defuse_button = msg.payload
-	else:
+	elif defuse_button == msg.payload:
 		print("killing process"+ str(timer_process_id))
-		status =subprocess.call(["sudo", "kill","-9",str(timer_process_id)])
-		print("status" + str(status))
+		status =os.killpg(os.getpgid(timer_process_id),signal.SIGTERM)
+		defuseSuccess()
+		defuse_button = None
+	else:
+		defuseFailure()	
+		time_delay = time.time() + 15
+    else:
+	print("ignoring button during lockout:" + msg.payload)
+
+def defuseFailure(): 
+	data = { "say":"Bomb defusal failure! Bomb defusal failure! Lock out for 15 seconds", "parms" : "-s 140 -ven-us+f3"}
+        
+	req = urllib2.Request('http://10.0.0.1:5000/say')
+	req.add_header('Content-Type', 'application/json')
+
+	response = urllib2.urlopen(req, json.dumps(data))
+
+def defuseSuccess(): 
+	data = { "say":"Bomb has been defused! Bomb has been defused! Stop, Stop, Stop, Stop stop stop", "parms" : "-s 140 -ven-us+f3"}
+        
+	req = urllib2.Request('http://10.0.0.1:5000/say')
+	req.add_header('Content-Type', 'application/json')
+
+	response = urllib2.urlopen(req, json.dumps(data))
+	time.sleep(1)
+	response = urllib2.urlopen(req, json.dumps(data))
+
 
 
 def blink():
---
+++ button_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python button_event.py 17 red &
+python button_event.py 23 white &
+python button_event.py 5 blue &
+python button_event.py 21 yellow &
+ 
